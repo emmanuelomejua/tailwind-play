28340bc4358e3aaff5d14d9a796f6e8560c0cba7
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-08-18 13:17:29 +0100

    form

--- src/comps/form/Form.jsx
+++ src/comps/form/Form.jsx
@@ -2,10 +2,38 @@ import './form.css'
 
 const Form = () => {
   return (
-    <main>
-      Form
+    <main className='form'>
+      <div className='progress'>
+        <label htmlFor='id'>
+          <p>34%</p>
+        </label>
+        <progress id='id' value={34} max={100}/>
+      </div>
+  
+      <section className='form__input'>
+        <div className='form__input-right'>
+          <input type='file'/>
+        </div>
+
+        <div className='form__input-left'>
+        <div className='form__cont'>
+             <input type='text' placeholder=''/>
+             <input type='text' placeholder=''/>
+
+             <div>
+              <input type='text' placeholder=''/>
+              <input type='text' placeholder=''/>
+             </div>
+
+            </div>
+        </div>
+      </section>
+
     </main>
   )
 }
 
+
+
 export default Form
+
