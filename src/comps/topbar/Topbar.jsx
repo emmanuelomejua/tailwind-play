02032d486c4c654087721805ba3a730e63b86403
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-08-18 11:05:33 +0100

    navbar completed

--- src/comps/topbar/Topbar.jsx
+++ src/comps/topbar/Topbar.jsx
@@ -1,21 +1,28 @@
 import { Add, Edit, Search } from '@mui/icons-material'
 import './topbar.css'
 import Logo from '../../constants/download.jpg'
+import { Link } from 'react-router-dom'
 
 const Topbar = () => {
   return (
     <nav className='navbar'>
         <section className='navbar__cont'>
+          <Link to='/'>
             <img src={Logo} alt="" className="logo" />
+          </Link>
 
             <div className='navber__search'>
-                <input type="text" className="" placeholder='Search Player...'/>
-                <Search/>
+                <input type="text" className="navber__search-input" placeholder='Search Player...'/>
+                <Search className='icon'/>
             </div>
 
             <div className='navber__right'>
-                <Add/>
-                <Edit/>
+              <Link to='/create'>
+                <Add className='icons'/>
+              </Link>
+              <Link to='/edit'>
+                <Edit className='icons'/>
+              </Link>
             </div>
         </section>
     </nav>
