dcec53df7e318c78fcbea4607185ee4b44e7383c
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-08-18 15:05:46 +0100

    committed

--- src/comps/table/Table.jsx
+++ src/comps/table/Table.jsx
@@ -46,6 +46,45 @@ const Table = () => {
                 <td>90</td>
             </section>
         </tr>
+      <tr className='tbody'>
+            <section>
+                <td>Player 1</td>
+            </section>
+
+            <section className='thead__key-child1'>
+                <td>23</td>
+                <td>75</td>
+                <td>45</td>
+                <td>30</td>
+                <td>90</td>
+            </section>
+        </tr>
+      <tr className='tbody'>
+            <section>
+                <td>Player 1</td>
+            </section>
+
+            <section className='thead__key-child1'>
+                <td>23</td>
+                <td>75</td>
+                <td>45</td>
+                <td>30</td>
+                <td>90</td>
+            </section>
+        </tr>
+      <tr className='tbody'>
+            <section>
+                <td>Player 1</td>
+            </section>
+
+            <section className='thead__key-child1'>
+                <td>23</td>
+                <td>75</td>
+                <td>45</td>
+                <td>30</td>
+                <td>90</td>
+            </section>
+        </tr>
 
       </tbody>
     </table>
