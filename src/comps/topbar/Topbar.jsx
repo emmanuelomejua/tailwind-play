5f04d4e79202ccc6048f6bee63281b1655bc5f3c
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-08-18 05:44:34 +0100

    progress made

--- src/comps/topbar/Topbar.jsx
+++ src/comps/topbar/Topbar.jsx
@@ -1,10 +1,24 @@
+import { Add, Edit, Search } from '@mui/icons-material'
 import './topbar.css'
+import Logo from '../../constants/download.jpg'
 
 const Topbar = () => {
   return (
-    <main>
-      <h1>Topbar</h1>
-    </main>
+    <nav className='navbar'>
+        <section className='navbar__cont'>
+            <img src={Logo} alt="" className="logo" />
+
+            <div className='navber__search'>
+                <input type="text" className="" placeholder='Search Player...'/>
+                <Search/>
+            </div>
+
+            <div className='navber__right'>
+                <Add/>
+                <Edit/>
+            </div>
+        </section>
+    </nav>
   )
 }
 
