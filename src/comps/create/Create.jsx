b9b2219635221f4c4dd89127bbdac7df2fb7af73
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-08-21 14:48:50 +0100

    sdgthk

--- src/comps/create/Create.jsx
+++ src/comps/create/Create.jsx
@@ -1,145 +1,165 @@
-import './create.css'
-import { useState } from 'react'
-import axios from 'axios'
+import "./create.css";
+import { useState } from "react";
+import axios from "axios";
 
 const Create = () => {
-    
-  const url = 'http://skoutwatch-dev-api.eu-west-3.elasticbeanstalk.com/api/v1/players'
+  const url =
+    "http://skoutwatch-dev-api.eu-west-3.elasticbeanstalk.com/api/v1/players";
 
 
-  
+
   const [attributes, setAttribute] = useState([
     {
-      attribute: 'Dribbling',
-      score: ''
+      attribute: "Dribbling",
+      score: "",
     },
     {
-      attribute: 'Pacing',
-      score: ''
+      attribute: "Pacing",
+      score: "",
     },
     {
-      attribute: 'Shooting',
-      score: ''
+      attribute: "Shooting",
+      score: "",
     },
     {
-      attribute: 'Passing',
-      score: ''
+      attribute: "Passing",
+      score: "",
     },
     {
-      attribute: 'Defending',
-      score: ''
+      attribute: "Defending",
+      score: "",
     },
     {
-      attribute: 'Physicality',
-      score: ''
+      attribute: "Physicality",
+      score: "",
     },
-])
+  ]);
 
-    const [file, setFile] = useState('')
+  // const [file, setFile] = useState("");
 
-    const [values, setValues] = useState({
-        first_name: '',
-        last_name: '',
-        email: '',
-        nin: ''
-    })
+  const [values, setValues] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    nin: "",
 
-    const handleTextInputChange = (e) => {
-        setValues({...values, [e.target.name]: e.target.value})
-    }
+  });
 
-    const handleAttrChange = (e) => {
-      setAttribute({...attributes, [e.target.name]: e.target.value})
-    }
+  const handleTextInputChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
-    console.log(attributes)
+  const handleAttrChange = (attributeName, newScore) => {
+    const updatedAttri = attributes.map((attr) => {
+      if (attr.attribute === attributeName) {
+        return { ...attr, score: newScore };
+      }
+      return attr;
+    });
+    setAttribute(updatedAttri);
+  };
 
-    const handleFormSubmit = async (e) => {
-      e.preventDefault()
-     const {first_name, last_name, email, nin} = values
-     const { Dribbling, Pacing, Shooting, Passing, Defending, Physicality } = attributes
-        try {
-            const res = await axios.post(url, {
-              first_name,
-              last_name,
-              email,
-              nin,
-    
-              
-            }, {
-              headers: {
-               'Content-Type': 'application/json'
-              }
-            })
-            res.data && window.location.replace('/')
+ 
 
-            console.log(res.data)
-        } catch (error) {
-            console.log(error.message)
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const { first_name, last_name, email, nin } = values;
+
+    try {
+      const res = await axios.post(
+        url,
+        {
+          first_name,
+          last_name,
+          email,
+          nin,
+          attributes
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-    }
+      );
+      res.data && window.location.replace("/");
 
+      console.log(res.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
-    <form className='form' onSubmit={handleFormSubmit}>
-    <section className='progress'>
-        <label htmlFor='Dribbling'>
-          <p>Dribbling</p>
-          <span>67</span>
-        </label>
+    <form className="form" onSubmit={handleFormSubmit}>
+      {attributes.map((attr, index) => (
+        <section className="progress">
+          <label htmlFor="Dribbling">
+            <p>{attr.attribute}</p>
+            <span>{attr.score}</span>
+          </label>
+          <input
+            type="range"
+            className=""
+            max={100}
+            min={0}
+            value={attr.score}
+            onChange={e => handleAttrChange(attr.attribute, e.target.value)}
+          />
+        </section>
+      ))}
 
-      </section>
-  
-      <section className='form__input'>
-        <div className='form__input-right'>
-          <input type='file' onChange={(e)=>setFile(e.target.value[0])}/>
+      <section className="form__input">
+        <div className="form__input-right">
+          <input type="file"  />
         </div>
 
-        <div className='form__input-left'>
-        <div className='form__cont'>
-             <input 
-             type='text' 
-             placeholder='First Name' 
-             name='first_name' 
-             required min={4}
-             onChange={handleTextInputChange}
-             />
-
-             <input 
-             type='text' 
-             placeholder='Last Name' 
-             name='last_name' 
-             required min={4}
-             onChange={handleTextInputChange}
-             />
-
-             <div className='form__cont-sec'>
-              <input 
-              type='email' 
-              placeholder='Email' 
-              name='email' 
-              className='inputs2' 
-              min={1}
+        <div className="form__input-left">
+          <div className="form__cont">
+            <input
+              type="text"
+              placeholder="First Name"
+              name="first_name"
               required
+              min={4}
               onChange={handleTextInputChange}
+            />
+
+            <input
+              type="text"
+              placeholder="Last Name"
+              name="last_name"
+              required
+              min={4}
+              onChange={handleTextInputChange}
+            />
+
+            <div className="form__cont-sec">
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                className="inputs2"
+                min={1}
+                required
+                onChange={handleTextInputChange}
               />
 
-              <input 
-              type='number' 
-              placeholder='NIN' 
-              name='nin' 
-              className='inputs2' 
-              min={1}
-              onChange={handleTextInputChange}
+              <input
+                type="number"
+                placeholder="NIN"
+                name="nin"
+                className="inputs2"
+                min={1}
+                onChange={handleTextInputChange}
               />
-             </div>
-
-            <button type='submit'>Create</button>
             </div>
+
+            <button type="submit">Create</button>
+          </div>
         </div>
       </section>
     </form>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
